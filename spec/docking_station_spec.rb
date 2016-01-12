a42a95e72977f499155b6215059a96ff1a8417c9
Colin McCulloch <mccolane@gmail.com>
2016-01-12 16:10:33 +0000

    Basic Capacity Implemented

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,5 @@
 require 'docking_station'
+require 'pry'
 
 describe DockingStation do
 
@@ -18,24 +19,23 @@ describe DockingStation do
     end
   end
 
+
   describe '#dock' do
     it 'raises an error when full' do
-      subject.dock(Bike.new)
+      20.times {subject.dock Bike.new }
       expect { subject.dock Bike.new }.to raise_error 'Docking station is full'
-
     end
-  end
+    it 'releases working bikes' do
+      bike = Bike.new
+      subject.dock(bike)
+      subject.release_bike
+      expect(bike).to be_working
+    end
 
-  it 'releases working bikes' do
-    bike = Bike.new
-    subject.dock(bike)
-    subject.release_bike
-    expect(bike).to be_working
-  end
-
-  it 'allows bikes to be docked' do
-    bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
+    it 'allows bikes to be docked' do
+      bike = Bike.new
+      expect(subject.dock(bike)).to eq [bike]
+    end
   end
 
   it 'responds to bike' do
