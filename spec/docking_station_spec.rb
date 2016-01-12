bd95f0e52bb919328328331b921c7be4b72d2120
Colin McCulloch <mccolane@gmail.com>
2016-01-12 15:13:37 +0000

    Raising Exceptions when docking

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,14 +1,47 @@
 require 'docking_station'
 
 describe DockingStation do
-  it 'responds to release_bike' do
-    expect(subject).to respond_to :release_bike
+
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+
+    it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
+
+    it 'responds to release_bike' do
+      expect(subject).to respond_to :release_bike
+    end
+  end
+
+  describe '#dock' do
+    it 'raises an error when full' do
+      subject.dock(Bike.new)
+      expect { subject.dock Bike.new }.to raise_error 'Docking station is full'
+
+    end
   end
 
   it 'releases working bikes' do
-    bike = subject.release_bike
+    bike = Bike.new
+    subject.dock(bike)
+    subject.release_bike
     expect(bike).to be_working
   end
+
+  it 'allows bikes to be docked' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it 'responds to bike' do
+    is_expected.to respond_to(:bike)
+  end
+
 
 
 end
